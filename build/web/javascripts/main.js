95dfcbc4ddafd4469b1b95dd4ecd5cdceeafcaea
MitchelHdz <sebastianvicky_@hotmail.com>
2015-10-22 23:59:08 -0500

    Working on landing page x2

---
+++ build/web/javascripts/main.js
@@ -0,0 +1,16 @@
+
+var close_popup = document.getElementById('btn-popup'); // Assumes element with id='button'
+
+function closePopup() {
+    console.log('test');
+    var div = document.getElementById('popup-login');
+    var background = document.getElementById('header') 
+    if (div.style.visibility !== 'hidden') {
+        div.style.visibility = 'hidden';
+        background.className= 'header';
+    }
+    else {
+        div.style.visibility = 'visible';
+        background.className= 'header blur-filter';
+    }
+};
